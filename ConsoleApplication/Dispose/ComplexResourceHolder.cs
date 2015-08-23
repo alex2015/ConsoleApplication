c8be4fcc4ebcee673f59104e0d0fd20f04895269
alex2015 <alex2015@rambler.ru>
2015-08-23 19:54:53 +0300

    commit

---
+++ ConsoleApplication/Dispose/ComplexResourceHolder.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Microsoft.Win32.SafeHandles;
+
+namespace ConsoleApplication.Dispose
+{
+    public class ComplexResourceHolder : IDisposable
+    {
+        // Буфер из неуправляемого кода (неуправляемый ресурс)    
+        private IntPtr _buffer;
+        // Дескриптор события ОС (управляемый ресурс)   
+        private SafeHandle _handle;
+
+        public ComplexResourceHolder()
+        {
+            // Захватываем ресурсы       
+            _buffer = AllocateBuffer();
+            _handle = new SafeWaitHandle(IntPtr.Zero, true);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            // Неуправляемые ресурсы освобождаются в любом случае       
+            ReleaseBuffer(_buffer);                   
+            // Вызываем из метода Dispose, освобождаем управляемые ресурсы 
+            if (disposing)
+            {
+                if (_handle != null)
+                {
+                    _handle.Dispose();
+                }
+            }
+        }
+
+        ~ComplexResourceHolder()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private IntPtr AllocateBuffer()
+        {
+            return new IntPtr();
+        }
+
+        private void ReleaseBuffer(IntPtr buffer)
+        {
+        }
+    }
+}
---
+++ ConsoleApplication/Dispose/ProperComplexResourceHolder.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Microsoft.Win32.SafeHandles;
+
+namespace ConsoleApplication.Dispose
+{
+    internal class ProperComplexResourceHolder : IDisposable
+    {
+        // Буфер из неуправляемого кода (неуправляемый ресурс)    
+        private IntPtr _buffer;
+        // Дескриптор события ОС (управляемый ресурс) 
+        private SafeHandle _handle;
+
+        public ProperComplexResourceHolder()
+        {
+            // Захватываем ресурсы       
+            _buffer = AllocateBuffer();
+            _handle = new SafeWaitHandle(IntPtr.Zero, true);
+        }
+
+        protected virtual void DisposeNativeResources()
+        {
+            ReleaseBuffer(_buffer);
+        }
+
+        protected virtual void DisposeManagedResources()
+        {
+            if (_handle != null)
+            {
+                _handle.Dispose();
+            }
+        }
+
+        ~ProperComplexResourceHolder()
+        {
+            DisposeNativeResources();
+        }
+
+        public void Dispose()
+        {
+            DisposeNativeResources(); 
+            DisposeManagedResources(); 
+            GC.SuppressFinalize(this);
+        }
+
+        private IntPtr AllocateBuffer()
+        {
+            return new IntPtr();
+        }
+
+        private void ReleaseBuffer(IntPtr buffer)
+        {
+        }
+    }
+}
