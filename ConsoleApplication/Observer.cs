175991cd8af698b2808881b9e6377a3e1c5d0dd7
alex2015 <alex2015@rambler.ru>
2015-09-04 15:38:14 +0300

    commit

---
+++ ConsoleApplication/Observer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace ConsoleApplication
+{
+    public class LogEntryEventArgs : EventArgs
+    {
+        public string LogEntry { get; internal set; }
+    }
+
+    public class LogFileReader
+    {
+        private readonly string _logFileName;
+        public LogFileReader(string logFileName)
+        {
+            _logFileName = logFileName;
+        }
+
+        public event EventHandler<LogEntryEventArgs> OnNewLogEntry;
+
+        private void RaiseNewLogEntry(string logEntry)
+        {
+            var handler = OnNewLogEntry;
+            if (handler != null)
+            {
+                handler(this, new LogEntryEventArgs());
+            }
+        }
+    }
+}
