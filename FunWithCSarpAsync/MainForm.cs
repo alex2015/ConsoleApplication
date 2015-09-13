3bf3b3a91ae0fb645c460bd07154de1ca0a1266a
alex2015 <alex2015@rambler.ru>
2015-09-13 18:29:29 +0300

    async/await part 1

---
+++ FunWithCSarpAsync/MainForm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FunWithCSarpAsync
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnCallMethod_Click(object sender, EventArgs e)
+        {
+            this.Text = await DoWork();
+        }
+
+        private Task<string> DoWork()
+        {
+            return Task.Run((Func<string>) MyMethod);
+        }
+
+        private string MyMethod()
+        {
+            Thread.Sleep(10000);
+            return "Done with work!";
+        }
+    }
+}
