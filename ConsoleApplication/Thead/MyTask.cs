4f6bc8a46e5382f216d2055e685e31647219fee8
alex2015 <alex2015@rambler.ru>
2015-10-25 16:39:22 +0300

    commit

--- ConsoleApplication/Thead/MyTask.cs
+++ ConsoleApplication/Thead/MyTask.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -8,7 +10,7 @@ namespace ConsoleApplication.Thead
     {
         public static void MyMain()
         {
-            MyMain5();
+            MyMain6();
         }
 
         #region MyMain1
@@ -162,7 +164,7 @@ namespace ConsoleApplication.Thead
 
         #endregion
 
-        #region MyMain4
+        #region MyMain5
 
         public static void MyMain5()
         {
@@ -194,6 +196,80 @@ namespace ConsoleApplication.Thead
             Int32 sum = 0;
             for (; n > 0; n--)
             {
+                checked
+                {
+                    sum += n;
+                } // при больших n появляется
+                // исключение System.OverflowException
+            }
+            return sum;
+        }
+
+        #endregion
+
+        #region MyMain6
+
+        public static void MyMain6()
+        {
+            Task parent = new Task(() =>
+            {
+                var cts = new CancellationTokenSource();
+                var tf = new TaskFactory<Int32>(cts.Token, TaskCreationOptions.AttachedToParent, TaskContinuationOptions.ExecuteSynchronously, TaskScheduler.Default);
+
+                // Задание создает и запускает 3 дочерних задания
+                var childTasks = new[]
+                {
+                    tf.StartNew(() => Sum6(cts.Token, 10000)),
+                    tf.StartNew(() => Sum6(cts.Token, 20000)),
+                    tf.StartNew(() => Sum6(cts.Token, Int32.MaxValue)) // Исключение OverflowException
+                };
+
+                // Если дочернее задание становится источником исключения,
+                // отменяем все дочерние задания
+                for (Int32 task = 0; task < childTasks.Length; task++)
+                {
+                    childTasks[task].ContinueWith(t => cts.Cancel(), TaskContinuationOptions.OnlyOnFaulted);
+                }
+
+                // После завершения дочерних заданий получаем максимальное
+                // возвращенное значение и передаем его другому заданию
+                // для вывода
+                tf.ContinueWhenAll(childTasks,
+                    completedTasks => 
+                        completedTasks.Where(t => !t.IsFaulted && !t.IsCanceled).Max(t => t.Result),
+                    CancellationToken.None)
+                    .ContinueWith(t => Console.WriteLine("The maximum is: " + t.Result),
+                        TaskContinuationOptions.ExecuteSynchronously);
+            });
+
+            // После завершения дочерних заданий выводим, в том числе, и необработанные исключения
+            parent.ContinueWith(p =>
+            {
+                // Текст помещен в StringBuilder и однократно вызван
+                // метод Console.WriteLine просто потому, что это задание
+                // может выполняться параллельно с предыдущим,
+                // и я не хочу путаницы в выводимом результате
+                var sb = new StringBuilder("The following exception(s) occurred:" + Environment.NewLine);
+                foreach (var e in p.Exception.Flatten().InnerExceptions)
+                {
+                    sb.AppendLine(" " + e.GetType().ToString());
+                }
+
+                Console.WriteLine(sb.ToString());
+            }, TaskContinuationOptions.OnlyOnFaulted);
+
+            // Запуск родительского задания, которое может запускать дочерние
+            parent.Start();
+
+            Console.ReadKey();
+        }
+
+        private static Int32 Sum6(CancellationToken ct, Int32 n)
+        {
+            Int32 sum = 0;
+            for (; n > 0; n--)
+            {
+                ct.ThrowIfCancellationRequested();
                 checked
                 {
                     sum += n;
