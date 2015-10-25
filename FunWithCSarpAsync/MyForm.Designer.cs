f0bdbddd406ac6dbb1c4e9f9e9dd0322d40b49e6
alex2015 <alex2015@rambler.ru>
2015-10-25 18:10:35 +0300

    commit

---
+++ FunWithCSarpAsync/MyForm.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace FunWithCSarpAsync
+{
+    partial class MyForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // MyForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "MyForm";
+            this.Text = "MyForm";
+            this.Load += new System.EventHandler(this.MyForm_Load);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.MyForm_MouseClick);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ FunWithCSarpAsync/MyForm.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FunWithCSarpAsync
+{
+    public partial class MyForm : Form
+    {
+        private readonly TaskScheduler _mSyncContextTaskScheduler;
+        private CancellationTokenSource _mCts;
+
+        public MyForm()
+        {
+            InitializeComponent();
+
+            _mSyncContextTaskScheduler = TaskScheduler.FromCurrentSynchronizationContext();
+            Text = "Synchronization Context Task Scheduler Demo";
+            Visible = true;
+            Width = 600;
+            Height = 100;
+        }
+
+        private void MyForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MyForm_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (_mCts != null)
+            {
+                // Операция начата, отменяем ее
+                _mCts.Cancel();
+                _mCts = null;
+            }
+            else
+            {
+                // Операция не начата, начинаем ее
+                Text = "Operation running";
+                _mCts = new CancellationTokenSource();
+                // Задание использует планировщик по умолчанию
+                // и выполняет поток из пула
+                Task<Int32> t = Task.Run(() => Sum(_mCts.Token, 20000), _mCts.Token);
+                // Эти задания используют планировщик контекста синхронизации
+                // и выполняются в потоке графического интерфейса
+                t.ContinueWith(task => 
+                    Text = "Result: " + task.Result, 
+                    CancellationToken.None, TaskContinuationOptions.OnlyOnRanToCompletion, _mSyncContextTaskScheduler);
+                t.ContinueWith(task => 
+                    Text = "Operation canceled", 
+                    CancellationToken.None, TaskContinuationOptions.OnlyOnCanceled, _mSyncContextTaskScheduler);
+                t.ContinueWith(task =>
+                    Text = "Operation faulted", 
+                    CancellationToken.None, TaskContinuationOptions.OnlyOnFaulted, _mSyncContextTaskScheduler);
+            }
+
+            //base.OnMouseClick(e);
+        }
+
+        private static Int32 Sum(CancellationToken ct, Int32 n)
+        {
+            Thread.Sleep(3000);
+            Int32 sum = 0;
+            for (; n > 0; n--)
+            {
+                ct.ThrowIfCancellationRequested();
+                checked
+                {
+                    sum += n;
+                } // при больших n появляется
+                // исключение System.OverflowException
+            }
+            return sum;
+        }
+    }
+}
