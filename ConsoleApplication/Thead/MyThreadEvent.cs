6a9bcb775d3bdcc9720b4667590b12e17ca367f4
alex2015 <alex2015@rambler.ru>
2015-12-05 22:22:46 +0300

    commit

---
+++ ConsoleApplication/Thead/MyThreadEvent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication.Thead
+{
+    public class MyThreadEvent
+    {
+        public static void MyMain()
+        {
+            MyMain1();
+        }
+
+        public static void MyMain1()
+        {
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
