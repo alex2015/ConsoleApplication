8696a229d78cdccc457f7238904456bcb23dbd69
alex2015 <alex2015@rambler.ru>
2016-04-03 10:14:48 +0300

    commit

---
+++ WpfAppAllCode/MainWindow.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfAppAllCode
+{
+    class MainWindow : Window
+    {
+        private Button btnExitApp = new Button();
+        public MainWindow(string windowTitle, int height, int width)
+        {
+            // Сконфигурировать кнопку
+            btnExitApp.Click += btnExitApp_Clicked;
+            btnExitApp.Content = "Exit Application";
+            btnExitApp.Height = 25;
+            btnExitApp.Width = 100;
+
+            this.AddChild(btnExitApp);
+
+            // Сконфигурируем окно
+            this.Title = windowTitle;
+            this.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            this.Height = height;
+            this.Width = width;
+
+            this.Closing += MainWindow_Closing;
+            this.Closed += MainWindow_Closed;
+        }
+
+        private void btnExitApp_Clicked(object sender, RoutedEventArgs e)
+        {
+            if ((bool) Application.Current.Properties["GodMode"])
+            {
+                MessageBox.Show("Cheater!");
+            }
+
+            this.Close();
+        }
+
+        private void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            var result = MessageBox.Show("Do you want to close without saving?", "My App", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+
+            if (result == MessageBoxResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void MainWindow_Closed(object sender, EventArgs e)
+        {
+            MessageBox.Show("See ya!");
+        }
+    }
+}
---
+++ WpfAppAllCode/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+
+namespace WpfAppAllCode
+{
+    class Program : Application
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Program app = new Program();
+            app.Startup += AppStartUp;
+            app.Exit += AppExit;
+
+            app.Run();
+        }
+
+        static void AppExit(object sender, ExitEventArgs e)
+        {
+            MessageBox.Show("App has exited");
+        }
+
+        static void AppStartUp(object sender, StartupEventArgs e)
+        {
+            Current.Properties["GodMode"] = false;
+
+            if (e.Args.Any(arg => arg.ToLower() == "/godmode"))
+            {
+                Current.Properties["GodMode"] = true;
+            }
+
+            var mainWindow = new MainWindow("My better WPF App!!!", 200, 300);
+
+            mainWindow.Show();
+        }
+    }
+}
