7e9c593e099435feff147e93d83065ea7e0baff4
alex2015 <alex2015@rambler.ru>
2016-04-17 02:02:04 +0300

    commit

--- WpfTesterApp/MainWindow.xaml.cs
+++ WpfTesterApp/MainWindow.xaml.cs
@@ -24,5 +24,15 @@ namespace WpfTesterApp
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("You clicked the button!");
+        }
+
+        private void MainWindow_OnMouseMove(object sender, MouseEventArgs e)
+        {
+            this.Title = e.GetPosition(this).ToString();
+        }
     }
 }
