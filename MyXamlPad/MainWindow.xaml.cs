06d10b115777b94143f1a79cba18c4f4103c49e4
alex2015 <alex2015@rambler.ru>
2016-04-17 17:30:50 +0300

    commit

--- MyXamlPad/MainWindow.xaml.cs
+++ MyXamlPad/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,5 +25,33 @@ namespace MyXamlPad
         {
             InitializeComponent();
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (File.Exists("YourXaml.xaml"))
+            {
+                txtXamlData.Text = File.ReadAllText("YourXaml.xaml");
+            }
+            else
+            {
+                txtXamlData.Text = "<Window xmlns=\"http://schemas.microsoft.com/winfx/2006/xaml/presentation\"\n" +
+                                   "xmlns:x=\"http://schemas.microsoft.com/winfx/2006/xaml\"\n" +
+                                   "Height=\"400\" Width=\"500\" WindowStartupLocation=\"CenterScreen\">\n" +
+                                   "<StackPanel>\n" +
+                                   "</StackPanel>\n" +
+                                   "</Window>";
+            }
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnViewXaml_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
     }
 }
