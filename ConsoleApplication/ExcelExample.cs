caedf647109fc8511844bcb35334237dd3f87c15
alex2015 <alex2015@rambler.ru>
2017-04-28 17:36:23 +0300

    spire xls example

---
+++ ConsoleApplication/ExcelExample.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Spire.Xls;
+
+namespace ConsoleApplication
+{
+    class ExcelExample
+    {
+        public static void Method()
+        {
+            Console.WriteLine(Object.Equals(null, null));
+
+            Console.WriteLine(Object.ReferenceEquals(null,null));
+
+            Console.ReadKey();
+        }
+    }
+}
