2770c6da8ec155f435d4e33cc9c3805deefe231d
alex2015 <alex2015@rambler.ru>
2017-09-07 21:16:18 +0300

    add Test

---
+++ ConsoleApplication/Dispose/Test.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApplication.Dispose
+{
+    class Test : IDisposable
+    {
+        // НЕ virtual
+        public void Dispose()
+        {
+            Dispose(true);
+            // Препятствует запуску финализатора
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                //Вызвать метод Dispose на других объектах, которыми владеет данный экземпляр.
+                // Здесь можно ссылаться на другие финализируемые объекты.
+            }
+
+            // Освободить неуправляемые ресурсы, которыми владеет (только) этот объект.
+            // ...
+        }
+
+        ~Test()
+        {
+            Dispose(false);
+        }
+    }
+}
